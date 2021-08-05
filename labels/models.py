45b004bfba9e7f726cee779c5dd3c3b8324cbcee
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2021-08-05 15:45:44 +0300

    First stage is done

--- labels/models.py
+++ labels/models.py
@@ -5,4 +5,3 @@ class PlacemarkModel(models.Model):
 	title = models.CharField(max_length=100, blank=False, unique=True)
 	latitude = models.FloatField(default=0.0, blank=False)
 	longitude = models.FloatField(default=0.0, blank=False)
-	color = models.IntegerField(default=0)
--- map_events/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MapEventsConfig(AppConfig):
-	default_auto_field = 'django.db.models.BigAutoField'
-	name = 'map_events'
