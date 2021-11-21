364e993d16c511f1b0dd7033982ee9d02d51a174
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2021-11-21 18:28:56 +0300

    Some garbage collect

--- mapserver/settings.py
+++ mapserver/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)z3q6(8pk^_)d&ghe%_#70&2jylio_0)h9ivtx2^=8o!f7p@*@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
